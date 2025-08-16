2ea4e5372c5dc747acf799864d36f314917d9b64
AlbianBala <jbbala06@gmail.com>
2025-08-16 16:32:22 +0530

    ServicesAndAboutUs

--- pneucon/src/app/app.component.ts
+++ pneucon/src/app/app.component.ts
@@ -3,11 +3,13 @@ import { RouterOutlet } from '@angular/router';
 import { AppMenuComponent } from './app-menu/app-menu.component';
 import { HomeComponent } from './home/home.component';
 import { FooterComponent } from './footer/footer.component';
+import { ServiceComponent } from './service/service.component';
+import { AboutUsComponent } from './about-us/about-us.component'; 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, AppMenuComponent, HomeComponent, FooterComponent],
+  imports: [RouterOutlet, AppMenuComponent, HomeComponent, FooterComponent, ServiceComponent, AboutUsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
