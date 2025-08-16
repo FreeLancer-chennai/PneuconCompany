ce8c0e1d4a21f7ca516f720f38825a6dab4bf7dc
AlbianBala <jbbala06@gmail.com>
2025-08-16 17:06:51 +0530

    WhatsappLinkedIn

--- pneucon/src/app/app.component.ts
+++ pneucon/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { AppMenuComponent } from './app-menu/app-menu.component';
 import { HomeComponent } from './home/home.component';
@@ -9,10 +10,29 @@ import { AboutUsComponent } from './about-us/about-us.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, AppMenuComponent, HomeComponent, FooterComponent, ServiceComponent, AboutUsComponent],
+  imports: [CommonModule, RouterOutlet, AppMenuComponent, HomeComponent, FooterComponent, ServiceComponent, AboutUsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'pneucon';
+  whatsappModalOpen = false;
+
+  openWhatsappModal() {
+    this.whatsappModalOpen = true;
+  }
+
+  closeWhatsappModal(event?: MouseEvent) {
+    if (!event || event.target === event.currentTarget) {
+      this.whatsappModalOpen = false;
+    }
+  }
+
+  startWhatsappChat() {
+    window.open('https://wa.me/1234567890', '_blank');
+    setTimeout(() => {
+      window.location.href = 'https://www.whatsapp.com/download/';
+    }, 1000);
+    this.whatsappModalOpen = false;
+  }
 }
